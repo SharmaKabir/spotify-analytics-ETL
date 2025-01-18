9d09760fefc6148ee2f050ef761c1a150f912256
Kabir Sharma <sharmakabir204@gmail.com>
2025-01-19 01:35:50 +0530

    hid secrets and client id

--- spotify_urls.py
+++ spotify_urls.py
@@ -2,9 +2,11 @@ import re
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import mysql.connector
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 sp=spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id='83fdcabb5af747099c7fb41eef106eef',
-    client_secret='acc26dd865824209877b1ab0d72ce587'
+    client_id=os.getenv(SPOTIFY_CLIENT_ID),
+    client_secret=os.getenv(SPOTIFY_CLIENT_SECRET)
                                                          
 ))
