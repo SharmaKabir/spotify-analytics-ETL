84302333f73d478696837a26a4f00afe2885aeaa
Kabir Sharma <sharmakabir204@gmail.com>
2025-01-19 01:58:25 +0530

    i am so done

--- spotify_urls.py
+++ spotify_urls.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 sp=spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=os.getenv(SPOTIFY_CLIENT_ID),
-    client_secret=os.getenv(SPOTIFY_CLIENT_SECRET)
-                                                         
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")                                                       
 ))
