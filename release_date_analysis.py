400c58becdb9dc59cdd2e741d7136c8c23608246
Kabir Sharma <sharmakabir204@gmail.com>
2025-03-30 22:46:08 +0530

    added update schema method

--- release_date_analysis.py
+++ release_date_analysis.py
@@ -7,3 +7,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
+
+load_dotenv()
+
+db_config={
+    'host': 'localhost',
+    'user': 'root',
+    'password': '12345678',
+    'database': 'spotify_db'
+}
+
+def update_schema():
+    """Add release_date column to spotify_tracks table"""
+    connection= mysql.connector.connect(**db_config)
+    cursor=connection.cursor()
+    cursor.execute("SHOW COLUMNS FROM spotify_tracks LIKE 'release_date'")
+    column_exists=cursor.fetchone()
+    if not column_exists:
+        cursor.execute("ALTER TABLE spotify_tracks ADD COLUMN release_date DATE")
+        print("column add done")
+    cursor.close()
+    connection.close()
